f777bc75f6d82801fbd8d36fe95890eb0ea2ed6e
Rusty Wagner <rusty.wagner@gmail.com>
2022-06-03 19:44:52 -0400

    Add qttranslations module

--- target_qt6_version.py
+++ target_qt6_version.py
@@ -4,5 +4,5 @@ msvc_build = "14.28"
 msvc_dir_name = "msvc2019_64"
 vs_version = "2019"
 min_macos = "10.15.7"
-qt_modules = ["qtbase", "qtsvg", "qtwayland", "qtimageformats", "qtdeclarative", "qttools"]
+qt_modules = ["qtbase", "qtsvg", "qtwayland", "qtimageformats", "qtdeclarative", "qttools", "qttranslations"]
 pyside_modules = ["Core", "Gui", "Widgets", "Svg", "DBus", "PrintSupport"]
