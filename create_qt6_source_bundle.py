1cc67b3262b08d9a7bb53dbf973e443e5ac51961
Rusty Wagner <rusty.wagner@gmail.com>
2022-06-10 17:49:35 -0400

    Fix source bundle to include PySide source

--- create_qt6_source_bundle.py
+++ create_qt6_source_bundle.py
@@ -17,12 +17,8 @@ from target_qt6_version import qt_version, qt_modules
 parser = argparse.ArgumentParser(description = "Create bundle for Qt 6 source code")
 parser.add_argument("--skip-clone", help="skip cloning the Qt 6 source code", action="store_true")
 parser.add_argument("--skip-clean", help="skip removing the Qt 6 source code", action="store_true")
-parser.add_argument("--patch", help="patch the source")
 parser.add_argument("--mirror", help="use source mirror", action="store")
 args = parser.parse_args()
-
-if args.patch:
-	args.patch = os.path.abspath(args.patch)
 
 base_dir = Path(__file__).resolve().parent
 qt_dir = base_dir / "build"
@@ -30,12 +26,20 @@ source_path = qt_dir / "src"
 qt_source_path = source_path / f"qt{qt_version}"
 artifact_path = base_dir / "artifacts"
 qt_patches_path = base_dir / 'qt_patches'
+pyside_patches_path = base_dir / 'pyside_patches'
+pyside_source_path = source_path / f"pyside{qt_version}"
 
 qt_patches = []
 for patch in sorted(qt_patches_path.iterdir()):
 	if patch.suffix == '.patch':
 		resolved_path = patch.resolve()
 		qt_patches.append(patch.resolve())
+
+pyside_patches = []
+for patch in sorted(pyside_patches_path.iterdir()):
+	if patch.suffix == '.patch':
+		resolved_path = patch.resolve()
+		pyside_patches.append(patch.resolve())
 
 mirror = []
 if args.mirror:
@@ -57,8 +61,11 @@ os.mkdir(source_path)
 os.mkdir(qt_source_path)
 
 print("\nActive Qt version is " + qt_version)
-if args.patch:
-	print("Apply patch " + args.patch)
+
+for patch in qt_patches:
+	print(f"Apply Qt patch: {patch}")
+for patch in pyside_patches:
+	print(f"Apply PySide patch: {patch}")
 
 if input("\nIs this correct (y/n)? ") != "y":
 	print("Aborted")
@@ -101,22 +108,37 @@ if not args.skip_clone:
 		print("Failed to check out submodules")
 		sys.exit(1)
 
-	patch_contents = ""
+	qt_patch_contents = ""
 	for patch in qt_patches:
 		print(f"\nApplying patch {patch}...")
 		if subprocess.call(["git", "apply", os.path.abspath(patch)], cwd=qt_source_path) != 0:
 			print("Failed to patch source")
 			sys.exit(1)
-		patch_contents += open(patch).read()
+		qt_patch_contents += open(patch).read()
 
-	if args.patch:
-		print("\nApplying user provided patch...")
-		if subprocess.call(["git", "apply", os.path.abspath(args.patch)], cwd=qt_source_path) != 0:
+	print("\nCloning pyside-setup...")
+	if args.mirror:
+		if subprocess.call(["git", "clone", f"{args.mirror}pyside-setup", pyside_source_path]) != 0:
+			print("Failed to clone PySide git repository")
+			sys.exit(1)
+	else:
+		if subprocess.call(["git", "clone", "https://codereview.qt-project.org/pyside/pyside-setup", pyside_source_path]) != 0:
+			print("Failed to clone PySide git repository")
+			sys.exit(1)
+	if subprocess.call(["git", "checkout", qt_version], cwd=pyside_source_path) != 0:
+		print("Failed to check out branch '{}'".format(qt_version))
+		sys.exit(1)
+
+	pyside_patch_contents = ""
+	for patch in pyside_patches:
+		print(f"\nApplying patch {patch}...")
+		if subprocess.call(["patch", "-p1", "-i", os.path.abspath(patch)], cwd=pyside_source_path) != 0:
 			print("Failed to patch source")
 			sys.exit(1)
-		patch_contents += open(args.patch).read()
+		pyside_patch_contents += open(patch).read()
 
-	open(os.path.join(artifact_path, f"qt{qt_version}.patch"), 'w').write(patch_contents)
+	open(os.path.join(artifact_path, f"qt{qt_version}.patch"), 'w').write(qt_patch_contents)
+	open(os.path.join(artifact_path, f"pyside{qt_version}.patch"), 'w').write(pyside_patch_contents)
 
 	print("Removing .git directories...")
 	if subprocess.call("find . -name .git | xargs rm -rf", shell=True, cwd=qt_source_path) != 0:
@@ -125,9 +147,15 @@ if not args.skip_clone:
 	if subprocess.call("find . -name .gitmodules | xargs rm -rf", shell=True, cwd=qt_source_path) != 0:
 		print("Failed to remove .gitmodules")
 		sys.exit(1)
+	if subprocess.call("find . -name .git | xargs rm -rf", shell=True, cwd=pyside_source_path) != 0:
+		print("Failed to remove .git directories")
+		sys.exit(1)
+	if subprocess.call("find . -name .gitmodules | xargs rm -rf", shell=True, cwd=pyside_source_path) != 0:
+		print("Failed to remove .gitmodules")
+		sys.exit(1)
 
 print("Compressing...")
-if subprocess.call(["tar", "cJf", os.path.join(artifact_path, f"qt{qt_version}.tar.xz"), f"qt{qt_version}"], cwd=source_path) != 0:
+if subprocess.call(["tar", "cJf", os.path.join(artifact_path, f"qt{qt_version}.tar.xz"), f"qt{qt_version}", f"pyside{qt_version}"], cwd=source_path) != 0:
 	print("Failed to compress source")
 	sys.exit(1)
 
