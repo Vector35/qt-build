77c21997fa0d9ac0d678806657149a61956715e3
Rusty Wagner <rusty.wagner@gmail.com>
2025-01-03 13:57:50 -0500

    Update to Qt 6.8.1

--- target_qt6_version.py
+++ target_qt6_version.py
@@ -1,8 +1,8 @@
-qt_version = "6.7.2"
+qt_version = "6.8.1"
 llvm_version = "16.0.0"
 msvc_build = "14.34"
 msvc_dir_name = "msvc2022_64"
 vs_version = "2022"
-min_macos = "11.0"
+min_macos = "12.0"
 qt_modules = ["qtbase", "qtsvg", "qtwayland", "qtimageformats", "qtdeclarative", "qttools", "qttranslations", "qtlanguageserver", "qtshadertools"]
 pyside_modules = ["Core", "Gui", "Widgets", "Svg", "DBus", "PrintSupport"]
